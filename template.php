18ad05d05087fb2c57c7cedbca16e413b978c072
valentinbus <valentinoiho@gmail.com>
2018-01-31 15:35:00 +0100

    tructure + regex

---
+++ template.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Test</title>
+</head>
+<body>
+    <div class = "cadre">
+    <form method="post">
+        Choisissez le type de tri : </br>
+        <select name="choix_tri">
+            <option value="tri_par_selection">Tri par selection</option>
+            <option value="tri_par_insertion">Tri par insertion</option>
+            <option value="tri_a_bulles">Tri à bulles</option>
+    
+        </select><br/>
+        <input type="submit" name="submit"/>
+    </form>
+
+    <?php
+    if (isset($_POST['choix_tri'])){
+        switch ($_POST['choix_tri']) {
+            case 'tri_par_selection':
+                echo "Vous avez choisi le tri par selection :";
+                break;
+            case 'tri_par_insertion' :
+                echo "Vous avez choisi le tri par insertion :";
+                break;
+            case 'tri_a_bulles' :
+                echo "Vous avez choisi le tri à bulles :";
+                break;
+        }
+    }
+    ?>
+</body>
+</html>
